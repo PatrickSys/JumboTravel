5f643779459d8b2de1a96572647b3e6b76522e46
bitaz <rossellocolompatrick@gmail.com>
2022-04-27 18:51:22 +0200

    Structure created

--- jumbo-travel/apps/jumbo-front/src/app/modules/page/page.module.ts
+++ jumbo-travel/apps/jumbo-front/src/app/modules/page/page.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { PageComponent } from './containers/components/page/page.component';
 import { SidenavComponent } from './containers/components/sidenav/sidenav.component';
 import { RouterModule } from "@angular/router";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../auth/services/auth.service';
 
 @NgModule({
   declarations: [PageComponent, SidenavComponent],
-  imports: [CommonModule, RouterModule]
+  providers : [
+    AuthService
+  ],
+  exports: [
+    PageComponent
+  ],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatIconModule]
 })
 export class PageModule {}
